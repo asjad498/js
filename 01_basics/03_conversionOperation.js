cb51f057859180c646236f8599692c7478e3ced4
asjad498 <159512846+asjad498@users.noreply.github.com>
2024-08-20 10:53:22 +0000

    stage all change 1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,39 @@ let someNumer = 33
 let stringNumber = String(someNumer)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ----------------------- Operations ----------------------- //
+
+let value =3 
+let negValue = -value
+console.log(negValue)
+
+console.log(2*3);
+
+let str1 = "asjad"
+let str2 = " khan"
+console.log(str1 + str2)
+
+console.log(2 + 2 % 3) 
+
+let prefixIncrement = 3
+++ prefixIncrement
+console.log(prefixIncrement);
+let postfixIncrement = 3
+postfixIncrement ++
+console.log(postfixIncrement);
+
+// the main difference in above two is as the name suggests
+
+// Code below for better Example
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:$x, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
